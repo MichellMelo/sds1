787fb7591bb0c3728a746ec341513a7453ef3979
Michell Melo <michellmelo58@gmail.com>
2020-09-28 16:43:29 -0300

    Finalizando app React Native

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { AppLoading } from 'expo';
 import {
   useFonts,
@@ -8,7 +8,6 @@ import {
   Play_700Bold
 } from '@expo-google-fonts/play';
 
-import Header from './src/components/Header';
 import Routes from './src/routes';
 
 export default function App() {
